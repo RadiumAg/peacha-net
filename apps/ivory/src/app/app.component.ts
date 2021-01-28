f9c26062c4baf0116bd4e3e632ae96cf3b3c76ff
kinori <kinori.me@gmail.com>
2021-01-28 10:33:08 +0800

    initial commit

---
+++ apps/ivory/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'peacha-net-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.less'],
+})
+export class AppComponent {
+  title = 'ivory';
+}
