d13d624bf97cdd57636cfd1cc16893277002b923
dmtlikebread <dingmt9614@163.com>
2021-03-10 10:44:33 +0800

    chore:新增企划撤回提示框

---
+++ apps/ivory/src/app/pages/commission/commission-detail/commission-pop-component/commission-prompt/commission-prompt.ts
@@ -0,0 +1,25 @@
+import { Component, Inject } from '@angular/core';
+import { ModalRef, MODAL_DATA_TOKEN } from '@peacha-core';
+
+@Component({
+    selector: 'ivo-commission-prompt',
+    templateUrl: './commission-prompt.html',
+    styleUrls: ['./commission-prompt.less'],
+})
+export class CommissionPrompt {
+
+    constructor(
+        private modalRef: ModalRef<CommissionPrompt>,
+        @Inject(MODAL_DATA_TOKEN) public key: { title: string; tips: string }
+    ) {
+    }
+
+
+    cancel(): void {
+        this.modalRef.close();
+    }
+
+    sure(): void {
+        this.modalRef.close(1);
+    }
+}
