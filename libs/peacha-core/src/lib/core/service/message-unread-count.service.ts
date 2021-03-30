f3c477ef6431dcdf1f75429a745346596fcb3e8f
kinori <admin@example.com>
2021-03-30 15:56:26 +0800

    活动页背景图更换，协议内容更新

--- libs/peacha-core/src/lib/core/service/message-unread-count.service.ts
+++ libs/peacha-core/src/lib/core/service/message-unread-count.service.ts
@@ -28,10 +28,14 @@ export class MessageUnreadCountService {
                             pms
                         }
                     }).subscribe(s => {
-                        this.replyCount$.next(Number(s.list.filter(l => l.platform === 'Peacha0')[0]?.count));
-                        this.likeCount$.next(Number(s.list.filter(l => l.platform === 'Peacha1')[0]?.count));
-                        this.systemCount$.next(Number(s.list.filter(l => l.platform === 'Peacha2')[0]?.count));
-                        this.bulletinCount$.next(Number(s.list.filter(l => l.platform === 'Peacha3')[0]?.count));
+                        // eslint-disable-next-line max-len
+                        this.replyCount$.next(s.list.filter(l => l.platform === 'Peacha0').length > 0 ? s.list.filter(l => l.platform === 'Peacha0')[0]?.count : 0);
+                        // eslint-disable-next-line max-len
+                        this.likeCount$.next(s.list.filter(l => l.platform === 'Peacha1').length > 0 ? s.list.filter(l => l.platform === 'Peacha1')[0]?.count : 0);
+                        // eslint-disable-next-line max-len
+                        this.systemCount$.next(s.list.filter(l => l.platform === 'Peacha2').length > 0 ? s.list.filter(l => l.platform === 'Peacha2')[0]?.count : 0);
+                        // eslint-disable-next-line max-len
+                        this.bulletinCount$.next(s.list.filter(l => l.platform === 'Peacha3').length > 0 ? s.list.filter(l => l.platform === 'Peacha3')[0]?.count : 0);
                         // eslint-disable-next-line max-len
                         this.allCount$.next(this.replyCount$.value + this.likeCount$.value + this.systemCount$.value + this.bulletinCount$.value);
                     })
