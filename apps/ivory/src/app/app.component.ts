16e0adc56a14f55150cfd4d566fcb280a0ab01c9
kinori <kinori.me@gmail.com>
2021-04-07 13:59:33 +0800

    chore: add dept

--- apps/ivory/src/app/app.component.ts
+++ apps/ivory/src/app/app.component.ts
@@ -3,6 +3,7 @@ import { Component, ChangeDetectionStrategy } from '@angular/core';
 import { OnInit } from '@angular/core';
 import { environment } from '../environments/environment';
 import { TranslateService } from '@ngx-translate/core';
+import { filter, map } from 'rxjs/operators';
 
 declare let gtag: (
 	eventName: string,
@@ -21,7 +22,28 @@ declare let gtag: (
 export class AppComponent implements OnInit {
 	title = 'ivory';
 
-	constructor(public router: Router,private translateService: TranslateService) {
+	showNavbar$ = this.router.events.pipe(
+		filter(event => event instanceof NavigationEnd),
+		map((event: NavigationEnd) => {
+			if (event.url.startsWith('/n7r')) {
+				return false;
+			}
+			return true;
+		})
+	);
+
+	showFooter$ = this.router.events.pipe(
+		filter(event => event instanceof NavigationEnd),
+		map((event: NavigationEnd) => {
+			console.log(event.urlAfterRedirects);
+			if (event.url.startsWith('/n7r')) {
+				return false;
+			}
+			return true;
+		})
+	);
+
+	constructor(public router: Router, private translateService: TranslateService) {
 		if (environment.production) {
 			this.router.events.subscribe(event => {
 				if (event instanceof NavigationEnd) {
@@ -34,10 +56,10 @@ export class AppComponent implements OnInit {
 	}
 
 	ngOnInit() {
-    this.translateService.addLangs(['zh', 'en', 'ja']);
-    this.translateService.setDefaultLang('zh');
-    const browserLang = this.translateService.getBrowserLang();
-    this.translateService.use(browserLang.match(/zh|en|ja/) ? browserLang : 'zh');
+		this.translateService.addLangs(['zh', 'en', 'ja']);
+		this.translateService.setDefaultLang('zh');
+		const browserLang = this.translateService.getBrowserLang();
+		this.translateService.use(browserLang.match(/zh|en|ja/) ? browserLang : 'zh');
 		window.onpopstate = (ev: PopStateEvent) => {
 			ev.cancelBubble = true;
 			ev.preventDefault();
--- apps/ivory/src/app/pages/n7r/n7r.module.ts
+++ apps/ivory/src/app/pages/n7r/n7r.module.ts
@@ -3,11 +3,17 @@ import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { N7rApiService } from './n7r-api.service';
 import { N7rPage } from './n7r.page';
+import { OverlayscrollbarsModule } from 'overlayscrollbars-ngx';
+import { N7rNavbarComponent } from './components/navbar.component';
+import { OverlayModule } from '@angular/cdk/overlay';
 
 @NgModule({
-	declarations: [N7rPage],
+	declarations: [N7rPage, N7rNavbarComponent],
 	imports: [
 		CommonModule,
+		OverlayscrollbarsModule,
+		OverlayModule,
+		RouterModule,
 		RouterModule.forChild([
 			{
 				path: '',
