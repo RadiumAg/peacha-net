264dc5476040b59a337d68b3e1ffd669c92c509a
dmtlikebread <dingmt9614@163.com>
2021-04-12 10:16:19 +0800

    chore:首页轮播和文案

--- apps/ivory/src/app/pages/n7r/n7r.page.ts
+++ apps/ivory/src/app/pages/n7r/n7r.page.ts
@@ -1,5 +1,5 @@
 import { animate, state, style, transition, trigger } from '@angular/animations';
-import { Component } from '@angular/core';
+import { ChangeDetectorRef, Component, HostListener } from '@angular/core';
 import { ModalService } from '@peacha-core';
 import { N7rGoodDetail } from './components/good-detail/good-detail';
 import { N7rPlay } from './components/play/play';
@@ -35,12 +35,15 @@ import { N7rApiService } from './n7r-api.service';
 export class N7rPage {
 	constructor(
 		private modal: ModalService,
-		private n7rApi: N7rApiService
+		private n7rApi: N7rApiService,
+		private cdr: ChangeDetectorRef
 	) {
-
+		this.current = 0;
+		this.autoPlay();
 	}
-
-
+	current: number;
+	clear: any;
+	imgs = ['/assets/image/n7r/banner/01.png', '/assets/image/n7r/banner/02.png'];
 	preOrder(type: number): void {
 		this.n7rApi.goodsList().subscribe(good => {
 			this.modal.open(N7rGoodDetail, { type, good })
@@ -48,7 +51,73 @@ export class N7rPage {
 
 	}
 
+	@HostListener('mouseenter', ['$event.target']) onMouseEnter() {
+		clearInterval(this.clear);
+	}
+	@HostListener('mouseout') onMouseOut() {
+		this.autoPlay();
+	}
+	@HostListener('click') onClick() {
+		clearInterval(this.clear);
+	}
+
+	ImgState(index: number) {
+		if (this.imgs && this.imgs.length) {
+			if (this.current === 0) {
+				return index === 0
+					? 'on'
+					: index === 1
+						? 'next'
+						: index === this.imgs.length - 1
+							? 'prev'
+							: 'off';
+			} else if (this.current === this.imgs.length - 1) {
+				return index === this.imgs.length - 1
+					? 'on'
+					: index === this.imgs.length - 2
+						? 'prev'
+						: index === 0
+							? 'next'
+							: 'off';
+			}
+			switch (index - this.current) {
+				case 0:
+					return 'on';
+				case 1:
+					return 'next';
+				case -1:
+					return 'prev';
+				default:
+					return 'off';
+			}
+		} else {
+			return 'off';
+		}
+	}
+
+
+	autoPlay() {
+		clearInterval(this.clear);
+		this.clear = setInterval(() => {
+			this.cdr.markForCheck();
+			if (this.current < this.imgs.length - 1) {
+				this.current++;
+			} else {
+				this.current = 0;
+			}
+		}, 5000);
+	}
+
 	toPlay(type: number): void {
 		this.modal.open(N7rPlay, type);
+	}
+
+	is_active(index: number) {
+		return index == this.current;
+	}
+
+	//下标切换
+	changeSmall(index: number) {
+		this.current = index;
 	}
 }
