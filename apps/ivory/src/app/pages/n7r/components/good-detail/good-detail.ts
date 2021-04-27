7e921d35cb16ceccf4b3f438f6c305af6f4dab3e
dmtlikebread <dingmt9614@163.com>
2021-04-27 16:20:33 +0800

    chore:暂存

--- apps/ivory/src/app/pages/n7r/components/good-detail/good-detail.ts
+++ apps/ivory/src/app/pages/n7r/components/good-detail/good-detail.ts
@@ -9,336 +9,280 @@ import { BehaviorSubject, Subject, Subscription, timer } from 'rxjs';
 import { switchMap, takeUntil, takeWhile, tap } from 'rxjs/operators';
 import { AddressSelect } from '../addressSelect/addressSelect';
 
-
 type Good = {
-    id: number;
-    price: number;
-    discountAmount: number;
-    name: string;
-    description: string;
-    stock: number;
-    salesVolumes: number;
-    cover: string;
-    month: number;
-}
-
+	id: number;
+	price: number;
+	discountAmount: number;
+	name: string;
+	description: string;
+	stock: number;
+	salesVolumes: number;
+	cover: string;
+	month: number;
+};
 
 @Component({
-    selector: 'ivo-n7r-good-detail',
-    templateUrl: './good-detail.html',
-    styleUrls: ['./good-detail.less']
+	selector: 'ivo-n7r-good-detail',
+	templateUrl: './good-detail.html',
+	styleUrls: ['./good-detail.less'],
 })
 export class N7rGoodDetail implements OnDestroy {
-    @Select(UserState.isLogin)
-    isLogin$: BehaviorSubject<boolean>;
+	@Select(UserState.isLogin)
+	isLogin$: BehaviorSubject<boolean>;
 
-    @ViewChild(Steps) steps: Steps;
+	@ViewChild(Steps) steps: Steps;
 
-    @ViewChild('selectInput') inputValue: HTMLInputElement;
+	@ViewChild('selectInput') inputValue: HTMLInputElement;
 
-    infoForm: FormGroup;
+	infoForm: FormGroup;
 
-    suitPic = [
-        '/assets/image/n7r/suit/2.png',
-        '/assets/image/n7r/suit/3.png',
-        '/assets/image/n7r/suit/7.png',
-        '/assets/image/n7r/suit/8.png',
-        '/assets/image/n7r/suit/9.png',
-        '/assets/image/n7r/single/1.png',
-        '/assets/image/n7r/single/5.png',
-        '/assets/image/n7r/single/6.png',
-        '/assets/image/n7r/single/8.png',
-        '/assets/image/n7r/single/9.png',
-    ];
+	suitPic = [
+		'/assets/image/n7r/suit/2.png',
+		'/assets/image/n7r/suit/3.png',
+		'/assets/image/n7r/suit/7.png',
+		'/assets/image/n7r/suit/8.png',
+		'/assets/image/n7r/suit/9.png',
+		'/assets/image/n7r/single/1.png',
+		'/assets/image/n7r/single/5.png',
+		'/assets/image/n7r/single/6.png',
+		'/assets/image/n7r/single/8.png',
+		'/assets/image/n7r/single/9.png',
+	];
 
-    btnType$ = new BehaviorSubject(0);
-    indexGood: Good;
+	btnType$ = new BehaviorSubject(1);
+	indexGood: Good;
 
-    indexChoice: number;
-    count$ = new BehaviorSubject(1);
-    destroy$ = new Subject<void>();
-    update$ = new BehaviorSubject(true);
+	count$ = new BehaviorSubject(1);
+	destroy$ = new Subject<void>();
+	update$ = new BehaviorSubject(true);
 
-    indexCity: any;
+	indexCity: any;
 
-    orderId: number;
-    createOrderTime: number;
+	orderId: number;
+	createOrderTime: number;
 
-    goodlist: any;
+	goodlist: any;
 
-    constructor(
-        private modalRef: ModalRef<N7rGoodDetail>,
-        @Inject(MODAL_DATA_TOKEN) public date: { type: number, good: { list: Array<Good> } },
-        private http: HttpClient,
-        private fb: FormBuilder,
-        private modal: ModalService,
-        private router: Router
-    ) {
+	constructor(
+		private modalRef: ModalRef<N7rGoodDetail>,
+		@Inject(MODAL_DATA_TOKEN) public date: { type: number; good: Good },
+		private http: HttpClient,
+		private fb: FormBuilder,
+		private modal: ModalService,
+		private router: Router
+	) {
+		this.indexGood = date.good;
+		this.infoForm = this.fb.group({
+			name: new FormControl('', [Validators.required, Validators.maxLength(20)]),
+			phone: new FormControl('', [Validators.required, Validators.pattern('^1[345789]\\d{9}$')]),
+			city: new FormControl('', [Validators.required]),
+			email: new FormControl('', [
+				Validators.required,
+				Validators.pattern('^\\w+((-\\w+)|(\\.\\w+))*@[A-Za-z0-9]+((\\.|-)[A-Za-z0-9]+)*\\.[A-Za-z0-9]+$'),
+				Validators.maxLength(50),
+			]),
+			address: new FormControl('', [Validators.required, Validators.maxLength(200)]),
+		});
+	}
 
+	get name(): AbstractControl {
+		return this.infoForm.get('name');
+	}
+	get phone(): AbstractControl {
+		return this.infoForm.get('phone');
+	}
+	get email(): AbstractControl {
+		return this.infoForm.get('email');
+	}
+	get address(): AbstractControl {
+		return this.infoForm.get('address');
+	}
+	get city(): AbstractControl {
+		return this.infoForm.get('city');
+	}
+	close(): void {
+		this.modalRef.close();
+	}
 
-        this.infoForm = this.fb.group(
-            {
-                name: new FormControl('', [Validators.required, Validators.maxLength(20)]),
-                phone: new FormControl('', [Validators.required, Validators.pattern('^1[345789]\\d{9}$')]),
-                city: new FormControl('', [Validators.required]),
-                email: new FormControl('', [
-                    Validators.required,
-                    Validators.pattern('^\\w+((-\\w+)|(\\.\\w+))*@[A-Za-z0-9]+((\\.|-)[A-Za-z0-9]+)*\\.[A-Za-z0-9]+$'),
-                    Validators.maxLength(50)]),
-                address: new FormControl('', [Validators.required, Validators.maxLength(200)]),
-            }
-        );
+	goto(): void {
+		this.isLogin$.subscribe(is => {
+			if (is) {
+				if (1618632000000 <= new Date().getTime()) {
+					if (this.btnType$.value === 1) {
+						if (this.infoForm.valid) {
+							this.http
+								.post<{ orderId: number }>(`/advance/create_order`, {
+									g: this.indexGood.id,
+									nu: this.count$.value,
+									pi: this.indexCity?.one?.id,
+									ci: this.indexCity?.two?.id,
+									coi: this.indexCity?.three?.id,
+									si: this.indexCity.four.id ?? '',
+									ad: this.infoForm.value.address,
+									ph: this.infoForm.value.phone,
+									e: this.infoForm.value.email,
+									n: this.infoForm.value.name,
+								})
+								.subscribe(
+									s => {
+										this.orderId = s.orderId;
+										this.steps.goto('pay');
+										this.btnType$.next(this.btnType$.value + 1);
+										this.createOrderTime = new Date().getTime();
+									},
+									e => {
+										if (e.code === 20000) {
+											this.modal
+												.open(PopTips, ['库存不足无法购买', false])
+												.afterClosed()
+												.subscribe(_ => {
+													this.modalRef.close();
+												});
+										} else if (e.code === 408) {
+											this.modal.open(PopTips, ['系统繁忙，请稍后重试', false]);
+										} else {
+											this.modal.open(PopTips, ['商品暂不可售', false]);
+										}
+									}
+								);
+						} else {
+							this.modal.open(PopTips, ['请填写完整有效的收货信息', false]);
+						}
+					} else {
+						this.modalRef.close();
+					}
+				} else {
+					this.modal.open(PopTips, ['2021年4月17日 12:00 开始预售', false]);
+				}
+			} else {
+				this.modalRef.close();
+				this.router.navigate(['login'], {
+					queryParams: {
+						return: 'n7r',
+					},
+				});
+			}
+		});
+	}
 
-        this.http.get<{
-            list: {
-                id: number;
-                price: number;
-                discountAmount: number;
-                name: string;
-                description: string;
-                stock: number;
-                salesVolumes: number;
-                cover: string;
-                month: number;
-            }[]
-        }>(`/advance/goods_list`).subscribe(good => {
-            this.goodlist = good;
-            this.indexChoice = this.date.type === 2 ? good.list.length - 1 : 0;
-            this.indexGood = good.list[this.indexChoice];
-        })
-    }
+	selectAddress(el: ElementRef): void {
+		this.modal
+			.openFloat(AddressSelect, el, null, true)
+			.afterClosed()
+			.subscribe((s: any) => {
+				this.indexCity = s;
+				this.city.setValue(s);
+			});
+	}
 
-    get name(): AbstractControl {
-        return this.infoForm.get('name');
-    }
-    get phone(): AbstractControl {
-        return this.infoForm.get('phone');
-    }
-    get email(): AbstractControl {
-        return this.infoForm.get('email');
-    }
-    get address(): AbstractControl {
-        return this.infoForm.get('address');
-    }
-    get city(): AbstractControl {
-        return this.infoForm.get('city');
-    }
-    close(): void {
-        this.modalRef.close();
-    }
+	pay(): void {
+		this.update$.subscribe(is => {
+			if (is) {
+				this.http
+					.post<{ payId: number }>('/shopmall/orders/pay', {
+						o: [this.orderId],
+						c: [],
+					})
+					.subscribe(s => {
+						this.http
+							.get<{
+								channelId: number;
+								page: string;
+							}>(`/trade/pay/cashier/launch?channelId=1&payId=${s.payId}`)
+							.subscribe(payResult => {
+								const div = document.createElement('divform');
+								div.innerHTML = payResult.page;
+								document.body.appendChild(div);
+								document.forms[0].setAttribute('target', '_blank');
+								document.forms[0].submit();
+								div.innerHTML = '';
 
-    goto(stock: number): void {
-        this.isLogin$.subscribe(is => {
-            if (is) {
-                if (1618632000000 <= new Date().getTime()) {
-                    if (this.btnType$.value === 0 && this.count$.value > 0) {
-                        if (stock > 0 && stock >= this.count$.value) {
-                            if (this.indexChoice === this.date.good.list.length - 1) {
-                                this.http.get(`/advance/check_order`).subscribe(_ => {
-                                    this.steps.goto('order');
-                                    this.btnType$.next(this.btnType$.value + 1);
-                                }, e => {
-                                    if (e.code === 403) {
-                                        this.modal.open(PopTips, ['需购买动捕套餐后才可购买单个追踪器', false]);
-                                    } else if (e.code === 20000) {
-                                        this.modal.open(PopTips, ['库存不足无法购买', false]);
-                                    }
-                                })
-                            } else {
-                                this.steps.goto('order');
-                                this.btnType$.next(this.btnType$.value + 1);
-                            }
-                        } else {
-                            this.modal.open(PopTips, ['库存不足无法购买', false]);
-                        }
+								//支付心跳
+								let heartbeatSub: Subscription;
+								if (heartbeatSub) {
+									heartbeatSub.unsubscribe();
+									heartbeatSub = undefined;
+								}
+								heartbeatSub = timer(1000, 3000)
+									.pipe(
+										switchMap(() =>
+											this.http.get<{
+												status: number;
+												cashiers: {
+													status: number;
+													error: string;
+													channelId: number;
+												}[];
+												error: string;
+											}>('/trade/pay/heartbeat', {
+												params: {
+													id: s.payId.toString(),
+												},
+											})
+										),
+										tap(heartbeat => {
+											if ([2, 4, 5].includes(heartbeat.status)) {
+												this.btnType$.next(-1);
+												this.steps.goto('success');
+											} else if ([3, 31, 32].includes(heartbeat.status)) {
+												// this.modal
+												//     .open(PopTips, [heartbeat.error, false])
+												//     .afterClosed()
+												//     .subscribe(_ => {
 
+												//     });
+												this.btnType$.next(-1);
+												this.steps.goto('fail');
+											}
+										}),
+										takeUntil(this.destroy$),
+										takeWhile(heartbeat => heartbeat.status === 0 || heartbeat.status === 1)
+									)
+									.subscribe();
+							});
+					});
+			} else {
+				this.modal.open(PopTips, ['支付已超时，请重新发起支付', false]);
+			}
+		});
+	}
 
-                    } else if (this.btnType$.value === 1) {
-                        if (this.infoForm.valid) {
+	orderTimeout() {
+		this.update$.next(false);
+	}
 
-                            this.http.post<{ orderId: number }>(`/advance/create_order`, {
-                                g: this.indexGood.id,
-                                nu: this.count$.value,
-                                pi: this.indexCity?.one?.id,
-                                ci: this.indexCity?.two?.id,
-                                coi: this.indexCity?.three?.id,
-                                si: this.indexCity.four.id ?? '',
-                                ad: this.infoForm.value.address,
-                                ph: this.infoForm.value.phone,
-                                e: this.infoForm.value.email,
-                                n: this.infoForm.value.name
-                            }).subscribe(s => {
-                                this.orderId = s.orderId;
-                                this.steps.goto('pay');
-                                this.btnType$.next(this.btnType$.value + 1);
-                                this.createOrderTime = new Date().getTime();
-                            }, e => {
-                                if (e.code === 20000) {
-                                    this.modal.open(PopTips, ['库存不足无法购买', false]).afterClosed().subscribe(_ => {
-                                        this.modalRef.close();
-                                    });
-                                } else if (e.code === 408) {
-                                    this.modal.open(PopTips, ['系统繁忙，请稍后重试', false]);
-                                } else {
-                                    this.modal.open(PopTips, ['商品暂不可售', false]);
-                                }
-                            })
+	cancel(): void {
+		this.steps.goto('cancel');
+		this.btnType$.next(3);
+	}
 
+	sureCancel(): void {
+		this.http
+			.post('/shopmall/orders/close', {
+				o: [this.orderId],
+			})
+			.subscribe(
+				s => {
+					this.modal
+						.open(PopTips, ['取消订单成功', false, 1])
+						.afterClosed()
+						.subscribe(_ => {
+							this.modalRef.close();
+						});
+				},
+				e => {
+					this.modal.open(PopTips, ['取消订单失败', false]);
+				}
+			);
+	}
 
+	notCancel(): void {
+		this.btnType$.next(2);
+		this.steps.goto('pay');
+	}
 
-                        } else {
-                            this.modal.open(PopTips, ['请填写完整有效的收货信息', false]);
-                        }
-
-                    } else {
-                        this.modalRef.close();
-                    }
-                } else {
-                    this.modal.open(PopTips, ['2021年4月17日 12:00 开始预售', false]);
-                }
-            } else {
-                this.modalRef.close();
-                this.router.navigate(['login'], {
-                    queryParams: {
-                        return: 'n7r',
-                    },
-                });
-            }
-        })
-
-
-
-    }
-
-    selectAddress(el: ElementRef): void {
-        this.modal.openFloat(AddressSelect, el, null, true).afterClosed().subscribe((s: any) => {
-            this.indexCity = s;
-            this.city.setValue(s);
-        })
-    }
-
-    pay(): void {
-        this.update$.subscribe(is => {
-            if (is) {
-                this.http
-                    .post<{ payId: number }>('/shopmall/orders/pay', {
-                        o: [this.orderId],
-                        c: []
-                    }).subscribe(s => {
-                        this.http.get<{
-                            channelId: number;
-                            page: string;
-                        }>(`/trade/pay/cashier/launch?channelId=1&payId=${s.payId}`).subscribe(
-                            payResult => {
-                                const div = document.createElement('divform');
-                                div.innerHTML = payResult.page;
-                                document.body.appendChild(div);
-                                document.forms[0].setAttribute('target', '_blank');
-                                document.forms[0].submit();
-                                div.innerHTML = '';
-
-
-                                //支付心跳
-                                let heartbeatSub: Subscription;
-                                if (heartbeatSub) {
-                                    heartbeatSub.unsubscribe();
-                                    heartbeatSub = undefined;
-                                }
-                                heartbeatSub = timer(1000, 3000)
-                                    .pipe(
-                                        switchMap(() => this.http.get<{
-                                            status: number;
-                                            cashiers: {
-                                                status: number;
-                                                error: string;
-                                                channelId: number;
-                                            }[];
-                                            error: string;
-                                        }>('/trade/pay/heartbeat', {
-                                            params: {
-                                                id: s.payId.toString(),
-                                            },
-                                        })),
-                                        tap(heartbeat => {
-                                            if ([2, 4, 5].includes(heartbeat.status)) {
-                                                this.btnType$.next(-1);
-                                                this.steps.goto('success');
-
-                                            } else if ([3, 31, 32].includes(heartbeat.status)) {
-                                                // this.modal
-                                                //     .open(PopTips, [heartbeat.error, false])
-                                                //     .afterClosed()
-                                                //     .subscribe(_ => {
-
-
-                                                //     });
-                                                this.btnType$.next(-1);
-                                                this.steps.goto('fail');
-                                            }
-                                        }),
-                                        takeUntil(this.destroy$),
-                                        takeWhile(heartbeat => heartbeat.status === 0 || heartbeat.status === 1)
-                                    )
-                                    .subscribe();
-                            }
-                        );
-                    })
-            } else {
-                this.modal.open(PopTips, ['支付已超时，请重新发起支付', false]);
-            }
-        })
-
-
-    }
-
-    orderTimeout() {
-        this.update$.next(false);
-    }
-
-    cancel(): void {
-        this.steps.goto('cancel');
-        this.btnType$.next(3);
-    }
-
-    sureCancel(): void {
-        this.http.post('/shopmall/orders/close', {
-            o: [this.orderId]
-        }).subscribe(s => {
-            this.modal.open(PopTips, ['取消订单成功', false, 1]).afterClosed().subscribe(_ => {
-                this.modalRef.close();
-            });
-        }, e => {
-            this.modal.open(PopTips, ['取消订单失败', false]);
-        })
-    }
-
-    notCancel(): void {
-        this.btnType$.next(2);
-        this.steps.goto('pay');
-    }
-
-
-
-    ngOnDestroy() {
-        this.destroy$.next();
-        this.destroy$.complete();
-    }
-
-    select(
-        item: Good,
-        index: number
-    ): void {
-        this.indexGood = item;
-        this.indexChoice = index;
-    }
-
-    changeValue(t: any): void {
-        if (t <= this.indexGood.stock - this.indexGood.salesVolumes) {
-            this.count$.next(this.indexGood.stock - this.indexGood.salesVolumes);
-        } else {
-            this.count$.next(t);
-        }
-
-    }
-
+	ngOnDestroy() {
+		this.destroy$.next();
+		this.destroy$.complete();
+	}
 }
--- apps/ivory/src/app/pages/n7r/n7r.page.ts
+++ apps/ivory/src/app/pages/n7r/n7r.page.ts
@@ -1,24 +1,26 @@
 import { animate, state, style, transition, trigger } from '@angular/animations';
+import { HttpClient } from '@angular/common/http';
 import { ChangeDetectorRef, Component, ElementRef, HostListener, ViewChild } from '@angular/core';
-import { ModalService } from '@peacha-core';
+import { Router } from '@angular/router';
+import { Select } from '@ngxs/store';
+import { ModalService, UserState } from '@peacha-core';
+import { PopTips } from '@peacha-core/components';
+import { BehaviorSubject } from 'rxjs';
 import { N7rGoodDetail } from './components/good-detail/good-detail';
 import { N7rPlay } from './components/play/play';
 import { N7rApiService } from './n7r-api.service';
 
-
-type Goods = {
-	list: {
-		id: number;
-		price: number;
-		discountAmount: number;
-		name: string;
-		description: string;
-		stock: number;
-		salesVolumes: number;
-		cover: string;
-		month: number;
-	}[]
-}
+type Good = {
+	id: number;
+	price: number;
+	discountAmount: number;
+	name: string;
+	description: string;
+	stock: number;
+	salesVolumes: number;
+	cover: string;
+	month: number;
+};
 
 @Component({
 	selector: 'ivo-n7r',
@@ -64,11 +66,17 @@ type Goods = {
 	],
 })
 export class N7rPage {
+	@Select(UserState.isLogin)
+	isLogin$: BehaviorSubject<boolean>;
+
 	@ViewChild('detail') content: ElementRef;
+
 	constructor(
 		private modal: ModalService,
 		private n7rApi: N7rApiService,
-		private cdr: ChangeDetectorRef
+		private cdr: ChangeDetectorRef,
+		private router: Router,
+		private http: HttpClient
 	) {
 		this.current = 0;
 		this.autoPlay();
@@ -76,24 +84,71 @@ export class N7rPage {
 			this.allGoods = good;
 			this.singleInfo = good.list.filter(l => Number(l.id) === 1145141007);
 			this.suitInfo = good.list.filter(l => Number(l.id) === 1145141006);
-		})
+		});
 	}
+
+	count$ = new BehaviorSubject(1);
+
+	indexChoice = 0;
+	indexGood: Good;
 
 	now = new Date().getTime();
 	current: number;
 	clear: any;
 	imgs = [
+		'/assets/image/n7r/banner/03.png',
+		'/assets/image/n7r/banner/04.png',
 		'/assets/image/n7r/banner/01.png',
 		'/assets/image/n7r/banner/02.png',
-		'/assets/image/n7r/banner/03.png',
-		'/assets/image/n7r/banner/04.png'
 	];
-	allGoods: Goods;
+	news = [
+		{ link: 'https://www.n7r.com.cn/nd.jsp?id=7', title: 'Tundra Tracker：比VIVE Tracker更轻更小更强大' },
+		{
+			link: 'https://www.n7r.com.cn/nd.jsp?id=5',
+			title: 'Exclusive: Tundra Tracker Aims for Smaller, Cheaper Alternative to Vive Tracker for SteamVR Tracking',
+		},
+		{ link: 'https://www.n7r.com.cn/nd.jsp?id=2', title: 'Valve 预告今年推出SteamVR 2.0 ' },
+		{ link: 'https://www.n7r.com.cn/nd.jsp?id=3', title: 'Lighthouse + Vive Tracker 的追踪有多牛？牛到可以在 VR 里玩杂耍' },
+		{ link: 'https://www.n7r.com.cn/nd.jsp?id=1', title: '最大支持16个2.0基站串联，Steam最新更新让大空间成本降低数十倍！' },
+		{ link: 'https://www.n7r.com.cn/nd.jsp?id=4', title: 'CES2020：概念产品走进生活' },
+	];
+	allGoods: { list: Good[] };
 	suitInfo: any = [];
 	singleInfo: any = [];
 
-	preOrder(type: number): void {
-		this.modal.open(N7rGoodDetail, { type, good: this.allGoods })
+	isZaoniao = false;
+
+	preOrder(stock: number): void {
+		this.isLogin$.subscribe(is => {
+			if (is) {
+				if (stock > 0 && stock >= this.count$.value) {
+					if (this.indexChoice == this.allGoods.list.length - 1) {
+						this.http.get(`/advance/check_order`).subscribe(
+							_ => {
+								this.modal.open(N7rGoodDetail, { type: this.isZaoniao, good: this.indexGood });
+							},
+							e => {
+								if (e.code === 403) {
+									this.modal.open(PopTips, ['需购买动捕套餐后才可购买单个追踪器', false]);
+								} else if (e.code === 20000) {
+									this.modal.open(PopTips, ['库存不足无法购买', false]);
+								}
+							}
+						);
+					} else {
+						this.modal.open(N7rGoodDetail, { type: this.isZaoniao, good: this.indexGood });
+					}
+				} else {
+					this.modal.open(PopTips, ['库存不足无法购买', false]);
+				}
+			} else {
+				this.router.navigate(['login'], {
+					queryParams: {
+						return: 'n7r',
+					},
+				});
+			}
+		});
 	}
 
 	@HostListener('mouseenter', ['$event.target']) onMouseEnter() {
@@ -112,26 +167,26 @@ export class N7rPage {
 				return index === 0
 					? 'on'
 					: index === 1
-						? 'next'
-						: index === 2
-							? 'rtwo'
-							: index === 3
-								? 'rthree'
-								: index === this.imgs.length - 1
-									? 'prev'
-									: 'off';
+					? 'next'
+					: index === 2
+					? 'rtwo'
+					: index === 3
+					? 'rthree'
+					: index === this.imgs.length - 1
+					? 'prev'
+					: 'off';
 			} else if (this.current === this.imgs.length - 1) {
 				return index === this.imgs.length - 1
 					? 'on'
 					: index === this.imgs.length - 2
-						? 'prev'
-						: index === this.imgs.length - 3
-							? 'ltwo'
-							: index === this.imgs.length - 4
-								? 'lthree'
-								: index === 0
-									? 'next'
-									: 'off';
+					? 'prev'
+					: index === this.imgs.length - 3
+					? 'ltwo'
+					: index === this.imgs.length - 4
+					? 'lthree'
+					: index === 0
+					? 'next'
+					: 'off';
 			}
 			switch (index - this.current) {
 				case 0:
@@ -181,11 +236,16 @@ export class N7rPage {
 		this.current = index;
 	}
 
-
 	goto(): void {
 		this.content.nativeElement.scrollIntoView({
 			behavior: 'smooth',
 			block: 'center',
 		});
+	}
+
+	select(item: Good, index: number): void {
+		this.indexGood = item;
+		this.indexChoice = index;
+		this.isZaoniao = index === this.allGoods.list.length - 1;
 	}
 }
